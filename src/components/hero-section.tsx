a0d8e59e917c04369a8a54173367c03249d28c62
Paulo Salvatore <salvatoregames@gmail.com>
2024-11-07 19:25:16 -0300

    chore(hero-section): remove old buttons

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -64,22 +64,6 @@ export default function HeroSection() {
             ))}
           </div>
         </div>
-
-        {/* <div className="flex w-full flex-col gap-2.5 sm:flex-row sm:justify-center">
-          <a
-            href="#"
-            className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base"
-          >
-            Start now
-          </a>
-
-          <a
-            href="#"
-            className="inline-block rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base"
-          >
-            Take tour
-          </a>
-        </div> */}
       </div>
 
       {/* social - start */}
