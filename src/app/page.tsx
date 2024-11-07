b9b84283f93430c49de7498c1bed40d23a214320
Paulo Salvatore <salvatoregames@gmail.com>
2024-11-07 20:03:51 -0300

    fix(home-page): replace `<a>` with `<Link />`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import HeroSection from "@/components/hero-section";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,7 +7,7 @@ export default function Home() {
       <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
         <header className="mb-8 flex items-center justify-between border-b py-4 md:mb-12 md:py-8 xl:mb-16">
           {/* logo - start */}
-          <a
+          <Link
             href="/"
             className="inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl"
             aria-label="logo"
@@ -21,8 +22,8 @@ export default function Home() {
             >
               <path d="M96 0V47L48 94H0V47L48 0H96Z" />
             </svg>
-            Flowrift
-          </a>
+            SA
+          </Link>
           {/* logo - end */}
 
           {/* nav - start */}
