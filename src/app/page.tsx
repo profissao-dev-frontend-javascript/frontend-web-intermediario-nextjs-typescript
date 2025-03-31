aaa37937921bead34bb4a9d863d06328e6f49c4b
Paulo Salvatore <salvatoregames@gmail.com>
2025-03-31 19:05:47 -0300

    refactor(header): extract component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 
+import Header from "@/components/Header";
 import HeroSection from "@/components/hero-section";
-import Image from "next/image";
-import Link from "next/link";
-import LogoSA from "/public/logo-sa.svg";
 // import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
@@ -29,28 +27,7 @@ export default function Home() {
   return (
     <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
       <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-        <header className="mb-8 flex items-center justify-between border-b py-4 md:mb-12 md:py-8 xl:mb-16">
-          {/* logo - start */}
-          <Link
-            href="/"
-            className="inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl"
-            aria-label="logo"
-          >
-            <Image src={LogoSA} alt="Logo Salvatore Academy" width={48} />
-            SA
-          </Link>
-          {/* logo - end */}
-
-          {/* buttons - start */}
-          <a
-            href="https://salvatore.academy"
-            target="_blank"
-            className="rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base"
-          >
-            Go to salvatore.academy 🔗
-          </a>
-          {/* buttons - end */}
-        </header>
+        <Header />
 
         <HeroSection />
       </div>
---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+import LogoSA from "/public/logo-sa.svg";
+
+export default function Header() {
+  return (
+    <header className="mb-8 flex items-center justify-between border-b py-4 md:mb-12 md:py-8 xl:mb-16">
+      {/* logo - start */}
+      <Link
+        href="/"
+        className="inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl"
+        aria-label="logo"
+      >
+        <Image src={LogoSA} alt="Logo Salvatore Academy" width={48} />
+        SA
+      </Link>
+      {/* logo - end */}
+
+      {/* buttons - start */}
+      <a
+        href="https://salvatore.academy"
+        target="_blank"
+        className="rounded-lg bg-gray-200 px-8 py-3 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base"
+      >
+        Go to salvatore.academy 🔗
+      </a>
+      {/* buttons - end */}
+    </header>
+  );
+}
